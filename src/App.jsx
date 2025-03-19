ecdb37c0fd9414d1c0add29242583d19b8111c93
jacobberggren <id23jbn@cs.umu.se>
2025-03-19 17:34:19 +0100

    adfsdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {HashRouter as Router, Route, Routes} from "react-router-dom";
 import {HelmetProvider} from 'react-helmet-async';
 import "leaflet/dist/leaflet.css";
 
@@ -21,7 +21,7 @@ import "./styles.css";
 function App() {
     return (
         <HelmetProvider>
-            <Router>
+            <Router basename="/">
                 <ToTop/>
                 <Header/>
                 <Routes>
